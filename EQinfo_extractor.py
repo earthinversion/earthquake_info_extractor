1602556cc2ea2509be0070f8829409220a91cf4d
utpalkumar <utpalkumar50@gmail.com>
2017-08-27 21:03:35 +0800

    initial commit

---
+++ EQinfo_extractor.py
@@ -0,0 +1,91 @@
+import os
+import glob
+import eventCatalogDownloader as ecd
+from datetime import datetime
+import re
+
+
+class eventnew:
+    now = datetime.now()
+    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+    def __init__(self, yrr, mon, day, hrr, mnn):
+        inp = [int(yrr), int(mon), int(day), int(hrr), int(mnn)]
+        catdirpath = "./catalogs"  # catalog directory
+        if 2005 <= inp[0] <= self.now.year:
+            catfile = self.months[int(mon) - 1] + str(yrr)[-2:] + ".ndk"
+            ftype = "ndk"
+        elif 1994 < inp[0] <= 2004:
+            catfile = self.months[int(mon) - 1] + str(yrr)[-2:] + ".dek"
+            ftype = "dek1"
+        elif inp[0] <= 1994:
+            catfile = "oldEQs.dek"
+            ftype = "dek2"
+        catfilefull = catdirpath + "/" + catfile
+        if not os.path.exists(catfilefull):
+            try:
+                ecd.catalogueDownload(inp[0], inp[1], catdirpath)
+            except:
+                print("The catalogue file is not available!")
+
+        for i in range(len(inp)):
+            if inp[i] < 10:
+                inp[i] = "%02d" % inp[i]
+            if not type(inp[i]) is str:
+                inp[i] = str(inp[i])
+
+        if ftype == "ndk":
+            dd = inp[0] + "/" + inp[1] + "/" + inp[2]  # yyyy/mm/dd
+            tmm = inp[3] + ":" + inp[4]  # hh:mm
+            eventRecogChar = ["PDE", "ISC", "SWE"]  # PDE for USGS location, ISC for ISC catalog, SWE for surface-wave location
+            with open(catfilefull) as catalog:
+                for line in catalog:
+                    if any(s in line for s in eventRecogChar):
+                        data = line.split()
+                        evtime1 = data[2].split(":")
+                        evtime2 = ":".join(evtime1[0:2])
+                        if dd == data[1] and tmm == evtime2:
+                            self.year, self.month, self.day = data[1].split("/")
+                            self.hour, self.min, self.sec = data[2].split(":")
+                            self.lat = data[3]
+                            self.long = data[4]
+                            self.depth = data[5]
+                            self.mag = data[6]
+                            self.name = " ".join(data[8:])
+        elif ftype == "dek1" or ftype == "dek2":
+            dd = inp[1] + "/" + inp[2] + "/" + inp[0][-2:]  # mm/dd/yy
+            tmm = inp[3] + ":" + inp[4]
+            rex = re.compile("^[A-Z][0-9]*[A-Z]\s")
+            with open(catfilefull, 'r') as file:
+                for line in file:
+                    if rex.match(line):
+                        data = line.split()
+                        evtime1 = data[2].split(":")
+                        evtime2 = ":".join(evtime1[0:2])
+                        if dd == data[1] and tmm == evtime2:
+                            self.month, self.day, self.year = data[1].split("/")
+                            self.hour, self.min, self.sec = data[2].split(":")
+                            self.lat = data[3]
+                            self.long = data[4]
+                            self.depth = data[5][0:4]
+                            self.mag = data[5][4:7]
+                            self.name = data[5][10:] + " " + " ".join(data[6:])
+
+    @classmethod
+    def from_id(cls, idd):
+        yr = idd[0:4]
+        mn = idd[4:6]
+        day = idd[6:8]
+        hr = idd[8:10]
+        mint = idd[10:12]
+        return cls(yr, mn, day, hr, mint)
+
+
+eqinfo = eventnew(1976, 3, 8, 4, 39)
+# eqinfo2 = eventnew.from_id("200504011527")
+print(eqinfo.lat)
+print(eqinfo.long)
+print(eqinfo.depth)
+print(eqinfo.mag)
+print(eqinfo.name)
+# 1976/01/01 01:29:39.6
---
+++ eventCatalogDownloader.py
@@ -0,0 +1,81 @@
+import requests
+import os
+import sys
+import numpy as np
+import re
+from datetime import datetime, date
+
+headerstr = '''This program gathers the event information from the Global CMT website
+http://www.globalcmt.org/CMTsearch.html
+Monthly catalogue is available for EQs from 1994-present.
+-Utpal Kumar
+##############################################################'''
+
+
+now = datetime.now()
+
+# Function for downloading the catalogue
+
+
+def catalogueDownload(year, month, loc):
+    try:
+        if year < 1976:
+            print("Catalog file is not available")
+            sys.exit()
+        yrsuffix = str(year)[-2:]
+        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+        filename = months[int(month) - 1] + yrsuffix
+
+        if 2005 <= year <= now.year:
+            url = 'http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/NEW_MONTHLY/%s/%s.ndk' % (str(year), filename)
+            ftype = ".ndk"
+            filename = filename + ftype
+        elif 1994 < year <= 2004:
+            url = "http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/MONTHLY/%s.dek" % (filename)
+            ftype = ".dek"
+            filename = filename + ftype
+        elif year <= 1994:
+            url = 'http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec04.dek'
+            ftype = ".dek"
+            filename = "oldEQs" + ftype
+        fullfilename = loc + "/" + filename
+        if not os.path.isdir(loc):
+            os.makedirs(loc)
+        if not os.path.exists(fullfilename):
+            print("Downloading the catalog file...")
+            r = requests.get(url)
+            with open(fullfilename, "wb") as file:
+                file.write(r.content)
+    except:
+        print("ERROR downloading the data")
+
+
+# catalogueDownload(2002, 4, './catalogs')
+if __name__ == "__main__":
+    print(headerstr, "\n")
+    yearrange = input("Enter the year/month range(1994-present)\nFormat:'yyyy/mm-yyyy/mm'\ne.g. '2003/01-2004/12': ")
+    # Checking for the input format
+    rex = re.compile("^[0-9]{4}/[0-9]{2}-[0-9]{4}/[0-9]{2}$")
+    if not rex.match(yearrange):
+        print("Please enter the correct format")
+        sys.exit()
+
+    years1 = yearrange.split("-")
+    yearS, monS = list(map(int, years1[0].split("/")))
+    yearE, monE = list(map(int, years1[1].split("/")))
+
+    if yearS > yearE or now.year < yearE:  # exit if the year entry is incorrect
+        print("Please enter proper format!")
+        sys.exit()
+    outdir = input("Enter the path to the download directory (else Enter for current directory): ")
+    if outdir == "":
+        outdir = "."
+
+    startTime = date(yearS, monS, 1)
+    endTime = date(yearE, monE, 1)
+
+    ym_start = 12 * startTime.year + startTime.month - 1
+    ym_end = 12 * endTime.year + endTime.month
+    for ym in np.arange(ym_start, ym_end):
+        y, m = divmod(ym, 12)
+        catalogueDownload(y, m, outdir)
